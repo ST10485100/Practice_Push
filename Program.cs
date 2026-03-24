3cf4e2d9bb2202c4bf3b5a71c976ebac6e5aefdf
ST10485100 <ST10485100@vcconnect.edu.za>
2026-03-24 10:15:18 +0200

    Hello, Olatunji

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Practice_Push_GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Olatunji");
+        }
+    }
+}
